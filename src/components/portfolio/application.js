0111716a7dc32fcd74bd970657a468b2433db7ee
Codename O'Connell <oconnellcoder@gmail.com>
2023-04-21 17:57:38 -0400

    started working on portfolio page

---
+++ src/components/portfolio/application.js
@@ -0,0 +1,42 @@
+import img1 from './images/Hey Boo-Boo site.png';
+import img2 from './images/homepage-screenshot.png';
+
+const apps = [
+    {
+        key: 1,
+        title: 'Hey Boo-Boo',
+        img: `${img1}`,
+        description: 'Test Test 1 2 3',
+        website: 'http://test.test.com'
+    },
+    {
+        key: 2,
+        title: 'Random Movie Night',
+        img: `${img2}`,
+        description: 'Test Test 1 2 3',
+        website: 'http://test.test.com'
+    },
+    {
+        key: 3,
+        title: 'Project 3',
+        img: `https://picsum.photos/200/200`,
+        description: 'Test Test 1 2 3',
+        website: 'http://test.test.com'
+    },
+    {
+        key: 4,
+        title: 'Project 4',
+        img: `https://picsum.photos/200/200`,
+        description: 'Test Test 1 2 3',
+        website: 'http://test.test.com'
+    },
+    {
+        key: 5,
+        title: 'Project 5',
+        img: `https://picsum.photos/200/200`,
+        description: 'Test Test 1 2 3',
+        website: 'http://test.test.com'
+    },
+]
+
+export default apps;
