9ef6d46c15dd445e87f575ad76be110714a66a63
Codename O'Connell <oconnellcoder@gmail.com>
2023-04-26 17:06:40 -0400

    worked on resume section

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Nav from "./nav";
 import About from "./about";
 import Portfolio from "./portfolio";
 import Contact from "./contact";
+import Resume from "./resume";
 
-export {Nav, About, Portfolio, Contact};
+export {Nav, About, Portfolio, Contact, Resume};
